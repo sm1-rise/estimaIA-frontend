001d8d1a31dce381d471eb5f9aa014f4dd7043cc
Samuel Martins <samuel.mrtins2016@gmail.com>
2024-12-08 23:52:58 -0300

    ajustando removendo componente não ultilizado

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,6 +1,5 @@
 import Header from "../../components/Header/Index";
 import Main from "../../components/Main/Index";
-import { GlobalMain } from "../../shared/GlobalStyle";
 
 export default function Home(){
     return (
